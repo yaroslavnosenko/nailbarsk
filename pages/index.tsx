d1af3000cdeb1a969212025bf5ddc7881806f481
ynosenko <yaroslav.nosenko@infobip.com>
2022-10-15 15:58:55 +0200

    added head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import { Box } from '@chakra-ui/react'
 import type { NextPage } from 'next'
-import { Header, Footer, Hero, Features, Map } from '../components'
+import { Header, Footer, Hero, Features, Map, Head } from '../components'
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head />
       <Header />
       <Box as="main">
         <Hero />
